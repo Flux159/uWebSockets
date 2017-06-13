47dd84475c6d82d0854d5f4fc59e60f43d9a01c4
Suyog Sonwalkar <suyog@chronotale.com>
2017-06-13 17:18:53 -0400

    Adding some stuff around deployment and starting work on install.js.

--- README.md
+++ README.md
@@ -71,3 +71,12 @@ If you wish to integrate with a specific event-loop you can define `USE_ASIO` or
 # Official bindings for µWebSockets
 * Node.js binding lives here, report issues with µWS itself over at uWebSockets repo. Report issues with the Node.js binding here.
 * Create new bindings and get them merged here.
+
+# Deployment Workflow: 
+Travis CI and Appveyor CI are setup for automatic continuous integration testing on commit. Publishing to NPM is also setup automatically on new tagged version.
+
+```shell
+git push # Push changes to master, CI runs automatically, make sure it works & passes all tests by checking Travis CI and Appveyor
+git tag -a v0.20.0 -m 'Version 0.20.0' # Create new tag
+git push --tags # Push tags independently after push to master, CI and deploy runs automatically
+```
--- nodejs/dist/install.js
+++ nodejs/dist/install.js
@@ -1,11 +1,18 @@
 
 var exec = require('child_process').exec;
+var packageJson = require('./package.json');
 
 // Run in cmd or in bash
 // https://stackoverflow.com/questions/32971416/cross-platform-npm-start-script
 // node-gyp rebuild > build_log.txt 2>&1 || exit 0
 var command_line = 'node-gyp rebuild > build_log.txt 2>&1 || exit 0';
 // var environ = (!process.argv[2].indexOf('development')) ? 'development' : 'production';
+
+var command = exec(command_line, function(error, stdout, stderr) {
+    // Download binary if command failed (using curl or powershell download file)
+
+    // Throw error to stderr if download and binary build failed
+});
 
 // if(process.platform === 'win32') {
 //   // tricks : https://github.com/remy/nodemon/issues/184#issuecomment-87378478 (Just don't add the space after the NODE_ENV variable, just straight to &&:)      
@@ -14,17 +21,20 @@ var command_line = 'node-gyp rebuild > build_log.txt 2>&1 || exit 0';
 //   command_line = 'NODE_ENV=' + environ + ' ' + command_line;
 // }
 
-var command = exec(command_line);
+// Just use curl or wget from command line
+// Windows wget in powershell is just an alias for another command:
+// https://stackoverflow.com/questions/36274084/how-to-execute-windows-powershell-command-using-childprocess-and-nodejs
+// https://superuser.com/questions/362152/native-alternative-to-wget-in-windows-powershell
 
-command.stdout.on('data', function(data) {
-  process.stdout.write(data);
-});
-command.stderr.on('data', function(data) {
-  process.stderr.write(data);
-});
-command.on('error', function(err) {
-  process.stderr.write(err);
-});
+// command.stdout.on('data', function(data) {
+//   process.stdout.write(data);
+// });
+// command.stderr.on('data', function(data) {
+//   process.stderr.write(data);
+// });
+// command.on('error', function(err) {
+//   process.stderr.write(err);
+// });
 
 // Need to handle cross platform download of binary if node-gyp fails
 // Technically would want to use node-pre-gyp or request module since those are
