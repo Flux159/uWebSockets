56859333d9d0a88a6ec87b22fe03f742c7658af9
Suyog Sonwalkar <suyog@chronotale.com>
2017-06-13 17:57:52 -0400

    Fixing some errors w/ install.js

--- nodejs/dist/install.js
+++ nodejs/dist/install.js
@@ -4,7 +4,10 @@ var path = require('path');
 var packageJson = require('./package.json');
 
 var command_line = 'node-gyp rebuild > build_log.txt 2>&1 || exit 0';
+// Without stderr redirection / always passing:
+// var command_line = 'node-gyp rebuild > build_log.txt';
 
+// TODO: Later could make this from package.json 'binary' so it matches node-pre-gyp
 var githubUsername = 'Flux159';
 var githubProjectName = 'uWebSockets';
 var version = 'v' + packageJson.version;
@@ -12,11 +15,12 @@ var platform = process.platform;
 var nodeVersion = process.versions.modules;
 
 var fileName = 'uws_' + platform +'_' + nodeVersion + '.node';
-var urlLink = 'https://github.com/' + githubProjectName + '/' +
-    projectName + '/releases/download/' + version + '/' + fileName;
+var urlLink = 'https://github.com/' + githubUsername + '/' +
+    githubProjectName + '/releases/download/' + version + '/' + fileName;
 var windowsPath = path.resolve(__dirname, './' + fileName);
 
-var curlCommand = 'curl -O ' + urlLink;
+// TODO: No idea if this handles proxies, redirects properly
+var curlCommand = 'curl -fLO ' + urlLink;
 // Note that powershell 3.0 allows you to use wget, but this command can work on
 // Powershell 2.0 (.NET 2.0)
 var powershellCommand = '(new-object System.Net.WebClient).DownloadFile( "'+
@@ -24,7 +28,7 @@ var powershellCommand = '(new-object System.Net.WebClient).DownloadFile( "'+
 
 var command = exec(command_line, function (error, stdout, stderr) {
     // Errors won't happen because 2>&1 and || exit 0 will almost always succeed
-    if (error) { }
+    if (error) {}
 
     // Download binary if command failed (using curl or powershell download file)
     // Note that currently this always downloads
